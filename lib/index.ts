e832263d5d18c7fad490927073546b4234e83884
Marlon Francisco <marlon@biggy.com.br>
2019-08-15 20:47:13 -0300

    Resolve cors

--- lib/index.ts
+++ lib/index.ts
@@ -4,6 +4,7 @@ import configs from "./configs";
 import AuthController from "./app/controllers/AuthController";
 import UserController from "./app/controllers/UserController";
 import LessonController from "./app/controllers/LessonController";
+import * as cors from "cors";
 
 const app = express();
 
@@ -14,9 +15,14 @@ const lessonRouter = new LessonController();
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
+app.use(cors());
 app.use("/auth", authRouter.Router);
 app.use("/user", userRouter.Router);
 app.use("/lesson", lessonRouter.Router);
+
+app.get("/", (req, res) => {
+    res.status(200).send("ok!");
+});
 
 app.listen(configs.port, () => console.log(`PORT: ${configs.port}`));
 
